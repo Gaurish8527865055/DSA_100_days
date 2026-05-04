e74f3865c1d2c36727562c4c1fa39dace7d16ee9
Gaurish8527865055 <gaurishydv@gmail.com>
2026-05-04 22:12:53 +0530

    Time: 25 ms (5.88%) | Memory: 20.2 MB (73.09%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.c
@@ -0,0 +1,86 @@
+typedef struct Node {
+    int val;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    Node* head;
+    int size;
+} MyLinkedList;
+
+MyLinkedList* myLinkedListCreate() {
+    MyLinkedList* obj = (MyLinkedList*)malloc(sizeof(MyLinkedList));
+    obj->head = NULL;
+    obj->size = 0;
+    return obj;
+}
+
+int myLinkedListGet(MyLinkedList* obj, int index) {
+    if (index < 0 || index >= obj->size) return -1;
+    Node* curr = obj->head;
+    for (int i = 0; i < index; i++) curr = curr->next;
+    return curr->val;
+}
+
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = obj->head;
+    obj->head = newNode;
+    obj->size++;
+}
+
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = NULL;
+    obj->size++;
+    if (!obj->head) {
+        obj->head = newNode;
+        return;
+    }
+    Node* curr = obj->head;
+    while (curr->next) curr = curr->next;
+    curr->next = newNode;
+}
+
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+    if (index < 0 || index > obj->size) return;
+    if (index == 0) {
+        myLinkedListAddAtHead(obj, val);
+        return;
+    }
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+    Node* curr = obj->head;
+    for (int i = 0; i < index - 1; i++) curr = curr->next;
+    newNode->next = curr->next;
+    curr->next = newNode;
+    obj->size++;
+}
+
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+    if (index < 0 || index >= obj->size) return;
+    Node* temp;
+    if (index == 0) {
+        temp = obj->head;
+        obj->head = obj->head->next;
+    } else {
+        Node* curr = obj->head;
+        for (int i = 0; i < index - 1; i++) curr = curr->next;
+        temp = curr->next;
+        curr->next = temp->next;
+    }
+    free(temp);
+    obj->size--;
+}
+
+void myLinkedListFree(MyLinkedList* obj) {
+    Node* curr = obj->head;
+    while (curr) {
+        Node* next = curr->next;
+        free(curr);
+        curr = next;
+    }
+    free(obj);
+}
